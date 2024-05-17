ab420d8cc87c44c4d203657699f7498075ce6f46
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-05-17 23:01:40 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,29 @@
+//  Q no 148
+
+setTimeout(() => {
+    console.log("This message is shown after a 2-second delay.");
+  }, 2000);
+
+//    Q no 149
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Callback executed"); 
+}, 0);
+
+console.log("End");
+
+//  Q no 150
+
+console.log("Before synchronous operation");
+
+for (let i = 0; i < 1e9; i++) {}
+console.log("After synchronous operation");
+
+
+console.log("Before asynchronous operation");
+setTimeout(() => {
+  console.log("Asynchronous operation completed");
+}, 1000);
+console.log("After asynchronous operation setup");
